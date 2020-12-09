e83eed8f7e5cacabab1d145b19319504a3b244c4
philippe.juhel@icam.fr <cAnberra7+>
2020-12-09 17:13:36 +0100

    Simplification de l'écriture

--- src/vpg_ros/threadActionRunner.py
+++ src/vpg_ros/threadActionRunner.py
@@ -27,42 +27,35 @@ class ThreadActionRunner(Thread):
         best_push_conf = np.max(self.push_predictions)
         best_grasp_conf = np.max(self.grasp_predictions)
         print('Primitive confidence scores: %f (push), %f (grasp)' % (best_push_conf, best_grasp_conf))
-        action_to_execute = 'grasp'
-        explore_actions = False
-        if best_push_conf > best_grasp_conf:
-            action_to_execute = 'push'
         explore_actions = np.random.uniform() < explore_prob
-        if explore_actions:  # Exploitation (do best action) vs exploration (do other action)
+        if explore_actions:  # Exploration (do random action)
             print('Strategy: explore (exploration probability: %f)' % explore_prob)
             action_to_execute = 'push' if np.random.randint(0, 2) == 0 else 'grasp'
-        else:
+        else: # Exploitation (do best action)
             print('Strategy: exploit (exploration probability: %f)' % explore_prob)
+            action_to_execute = 'push' if best_push_conf > best_grasp_conf else 'grasp'
         self.trainer.is_exploit_log.append([0 if explore_actions else 1])
         self.logger.write_to_log('is-exploit', self.trainer.is_exploit_log)
-        # Get pixel location and rotation with highest affordance prediction from heuristic algorithms (rotation, y, x)
+        # Get pixel location and rotation with highest affordance prediction from heuristic algorithms (rotation, y, x) => where should the action take place
         if action_to_execute == 'push':
-            self.output_variables['best_pix_ind'] = np.unravel_index(np.argmax(self.push_predictions), self.push_predictions.shape)
+            best_pix_ind = np.unravel_index(np.argmax(self.push_predictions), self.push_predictions.shape)
             predicted_value = np.max(self.push_predictions)
         elif action_to_execute == 'grasp':
-            self.output_variables['best_pix_ind'] = np.unravel_index(np.argmax(self.grasp_predictions), self.grasp_predictions.shape)
+            best_pix_ind = np.unravel_index(np.argmax(self.grasp_predictions), self.grasp_predictions.shape)
             predicted_value = np.max(self.grasp_predictions)
-        self.trainer.use_heuristic_log.append([0])
-        self.logger.write_to_log('use-heuristic', self.trainer.use_heuristic_log)
+        print('Action: %s at (%d, %d, %d)' % (action_to_execute, best_pix_ind[0], best_pix_ind[1],best_pix_ind[2]))
         # Save predicted confidence value
         self.trainer.predicted_value_log.append([predicted_value])
         self.logger.write_to_log('predicted-value', self.trainer.predicted_value_log)
         # Compute 3D position of pixel
-        print('Action: %s at (%d, %d, %d)' % (
-            action_to_execute, self.output_variables['best_pix_ind'][0], self.output_variables['best_pix_ind'][1],
-            self.output_variables['best_pix_ind'][2]))
-        best_rotation_angle = np.deg2rad(self.output_variables['best_pix_ind'][0] * (360.0 / self.trainer.model.num_rotations))
-        best_pix_x = self.output_variables['best_pix_ind'][2]
-        best_pix_y = self.output_variables['best_pix_ind'][1]
+        best_rotation_angle = np.deg2rad(best_pix_ind[0] * (360.0 / self.trainer.model.num_rotations))
+        best_pix_x = best_pix_ind[2]
+        best_pix_y = best_pix_ind[1]
         primitive_position = [best_pix_x * self.heightmap_resolution + self.workspace_limits[0][0],
                               best_pix_y * self.heightmap_resolution + self.workspace_limits[1][0],
                               self.valid_depth_heightmap[best_pix_y][best_pix_x] + self.workspace_limits[2][0]]
         # If pushing, adjust start position, and make sure z value is safe and not too low
-        if action_to_execute == 'push':  # or action_to_execute == 'place':
+        if action_to_execute == 'push':
             finger_width = 0.02
             safe_kernel_width = int(np.round((finger_width / 2) / self.heightmap_resolution))
             local_region = self.valid_depth_heightmap[
@@ -74,34 +67,28 @@ class ThreadActionRunner(Thread):
                 safe_z_position = np.max(local_region) + self.workspace_limits[2][0]
             primitive_position[2] = safe_z_position
         # Save executed primitive
-        if action_to_execute == 'push':
-            self.trainer.executed_action_log.append(
-                [0, self.output_variables['best_pix_ind'][0], self.output_variables['best_pix_ind'][1], self.output_variables['best_pix_ind'][2]])  # 0 - push
-        elif action_to_execute == 'grasp':
-            self.trainer.executed_action_log.append(
-                [1, self.output_variables['best_pix_ind'][0], self.output_variables['best_pix_ind'][1], self.output_variables['best_pix_ind'][2]])  # 1 - grasp
+        self.trainer.executed_action_log.append([1 if action_to_execute == 'grasp' else 0, best_pix_ind[0], best_pix_ind[1], best_pix_ind[2]])   # 0 - push, 1 - grasp
         self.logger.write_to_log('executed-action', self.trainer.executed_action_log)
         # Visualize executed primitive, and affordances
         if self.save_visualizations:
-            push_pred_vis = self.trainer.get_prediction_vis(self.push_predictions, self.color_heightmap, self.output_variables['best_pix_ind'])
+            push_pred_vis = self.trainer.get_prediction_vis(self.push_predictions, self.color_heightmap, best_pix_ind)
             self.logger.save_visualizations(self.trainer.iteration, push_pred_vis, 'push')
             cv2.imwrite('visualization.push.png', push_pred_vis)
-            grasp_pred_vis = self.trainer.get_prediction_vis(self.grasp_predictions, self.color_heightmap, self.output_variables['best_pix_ind'])
+            grasp_pred_vis = self.trainer.get_prediction_vis(self.grasp_predictions, self.color_heightmap, best_pix_ind)
             self.logger.save_visualizations(self.trainer.iteration, grasp_pred_vis, 'grasp')
             cv2.imwrite('visualization.grasp.png', grasp_pred_vis)
-        # Initialize variables that influence reward
-        self.output_variables['push_success'] = False
-        self.output_variables['grasp_success'] = False
-        change_detected = False
         # Execute primitive
         if action_to_execute == 'push':
             self.output_variables['push_success'] = self.push(primitive_position, best_rotation_angle)
+            self.output_variables['grasp_success'] = False
             print('Push successful: %r' % (self.output_variables['push_success']))
         elif action_to_execute == 'grasp':
             self.output_variables['grasp_success'] = self.grasp(primitive_position, best_rotation_angle)
+            self.output_variables['push_success'] = False
             print('Grasp successful: %r' % (self.output_variables['grasp_success']))
         # Output data for main thread (mainSimulation)
         self.output_variables['primitive_action'] = action_to_execute
+        self.output_variables['best_pix_ind'] = best_pix_ind
         time.sleep(0.01)
 
     def push(self, position, angle):
